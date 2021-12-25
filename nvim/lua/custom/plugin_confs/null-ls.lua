9cc7db8ebc3774e169a3c3db10b5d8c0f559fae2
Ang Kosal <angkosal@gmail.com>
2021-12-25 10:41:04 +0700

    Update null-ls config

--- nvim/lua/custom/plugin_confs/null-ls.lua
+++ nvim/lua/custom/plugin_confs/null-ls.lua
@@ -36,10 +36,17 @@ local sources = {
 local M = {}
 
 M.setup = function(on_attach)
-   null_ls.config {
-      sources = sources,
-   }
-   require("lspconfig")["null-ls"].setup { on_attach = on_attach }
+  null_ls.setup {
+    debug = true,
+    sources = sources,
+
+    -- format on save
+    on_attach = function(client)
+       if client.resolved_capabilities.document_formatting then
+          vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()"
+       end
+    end,
+  }
 end
 
 return M
