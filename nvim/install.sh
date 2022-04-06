b967dbd0fb2ad66e93d5814f729d653d8066ef0b
Ang Kosal <angkosal@gmail.com>
2022-04-06 18:39:47 +0700

    Update nvim install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,15 +5,9 @@
 # sudo apt install -y jq        => for res.nvim: https://github.com/NTBBloodbath/rest.nvim
 
 ### Check if a directory does not exist ###
-if [ ! -d "$OUT/.config/nvim" ] 
-then
-  echo "clonning NvChad..."
-  git clone "https://github.com/NvChad/NvChad.git" "$OUT/.config/nvim"
-fi
+#if [ ! -d "$OUT/.config/nvim" ]
+#then
+#  echo "clonning..."
+#fi
 
-# "$S" "$HEREP" "$OUT/.config/nvim"
-echo "symlink NvChad config"
-"$S" "$HEREP/lua/custom/init.lua" "$OUT/.config/nvim/lua/custom/init.lua"
-"$S" "$HEREP/lua/custom/chadrc.lua" "$OUT/.config/nvim/lua/custom/chadrc.lua"
-"$S" "$HEREP/lua/custom/plugins.lua" "$OUT/.config/nvim/lua/custom/plugins.lua"
-"$S" "$HEREP/lua/custom/plugin_confs" "$OUT/.config/nvim/lua/custom/plugin_confs"
+"$S" "$HEREP" "$OUT/.config/nvim"
