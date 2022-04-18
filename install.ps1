9ad095b5b956283c7f6b41f6d751dd1e8e085b83
Ang Kosal <angkosal@gmail.com>
2022-04-18 15:28:12 +0700

    Update window power shell config

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
 New-Item -ItemType SymbolicLink -Path "$HOME/AppData/Local/nvim" -Target "$HOME/dotfiles/nvim"
-# New-Item -ItemType SymbolicLink -Path "$HOME/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1" -Target "$HOME/dotfiles/powershell/profile.ps1"
+New-Item -ItemType SymbolicLink -Path "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1" -Target "$HOME/dotfiles/powershell/profile.ps1"
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,12 +1,16 @@
+Invoke-Expression (&starship init powershell)
+
 function Git-Status { git status }
+function Git-Add { git add $args }
 function Git-Diff { git diff }
 function Git-Log { git log }
-function Git-Commit { git commit }
-oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/powerlevel10k_classic.omp.json | Invoke-Expression
+function Git-Commit { git commit $args }
+function Qrcode-Gen {
+    python "$HOME\dotfiles\qrcode\qc.py" $args
+    start "$HOME\qrcode.png"
+}
 New-Alias lg lazygit
 New-Alias gs Git-Status
 New-Alias gd Git-Diff
-
-
-# New-Alias gl Git-Log
-# New-Alias gm Git-Commit
+New-Alias ga Git-Add
+New-Alias qr Qrcode-Gen
