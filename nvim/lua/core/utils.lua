429e3ba432245e5c9b6ad686ec481d368a0bbede
Ang Kosal <angkosal@gmail.com>
2022-05-02 11:09:59 +0700

    Update close buffer function

--- nvim/lua/core/utils.lua
+++ nvim/lua/core/utils.lua
@@ -3,7 +3,11 @@ local cmd = vim.cmd
 local fn = vim.fn
 
 M.close_buffer = function(force)
-  if force or not vim.bo.buflisted then cmd(":bd!") else cmd("bd") end
+  if vim.bo.buftype == "terminal" then vim.api.nvim_win_hide(0) return end
+  force = force or not vim.bo.buflisted or vim.bo.buftype == "nofile"
+  -- if not force, change to prev buf and then close current
+  local close_cmd = force and ":bd!" or ":bp | bd" .. vim.fn.bufnr()
+  vim.cmd(close_cmd)
 end
 
 M.tabline = function()
