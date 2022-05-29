913730a32e42ca619ae9d62896c1e0d1c7fbb420
Ang Kosal <angkosal@gmail.com>
2022-05-29 08:20:07 +0700

    Update nvim tree config

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -13,52 +13,16 @@
 --- Default options ARE NOT included.
 --- See: `:help NvimTree`
 
--- Global options
-local g = vim.g
-
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_git_hl = 0
-g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-
-g.nvim_tree_show_icons = {
-   folders = 1,
-   files = 1,
-   git = 1,
-}
-
-g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "✓",
-      unmerged = "",
-      unstaged = "✗",
-      untracked = "★",
-   },
-   folder = {
-      default = "",
-      empty = "",
-      empty_open = "",
-      open = "",
-      symlink = "",
-      symlink_open = "",
-   },
-}
-
 -- Call setup
 --- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require('nvim-tree').setup {
   filters = {
     dotfiles = false,
+    exclude = { "custom" },
   },
   disable_netrw = true,
   hijack_netrw = true,
-  ignore_ft_on_setup = { "dashboard" },
-  auto_close = false,
+  ignore_ft_on_setup = { "alpha" },
   open_on_tab = false,
   hijack_cursor = true,
   hijack_unnamed_buffer_when_opening = false,
@@ -68,24 +32,59 @@ require('nvim-tree').setup {
     update_cwd = false,
   },
   view = {
-    allow_resize = true,
     side = "left",
     width = 25,
     hide_root_folder = true,
   },
   git = {
     enable = false,
-    ignore = false,
+    ignore = true,
   },
   actions = {
     open_file = {
-       resize_window = true,
+      resize_window = true,
     },
   },
   renderer = {
+    highlight_git = false,
+    highlight_opened_files = "none",
+
     indent_markers = {
-      enable = true
-    }
-  }
+      enable = false,
+    },
+    icons = {
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = false,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          default = "",
+          empty = "",
+          empty_open = "",
+          open = "",
+          symlink = "",
+          symlink_open = "",
+          arrow_open = "",
+          arrow_closed = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
 }
 
