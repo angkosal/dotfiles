b9ca84dcb9a98ee0090db5fe9f6b9e7b1cf39fb1
Ang Kosal <angkosal@gmail.com>
2022-05-31 19:51:38 +0700

    Add nvim-treesitter-textobjects

--- nvim/lua/packer_init.lua
+++ nvim/lua/packer_init.lua
@@ -40,7 +40,8 @@ return packer.startup(function()
   use 'liuchengxu/vista.vim'
 
   -- Treesitter interface
-  use 'nvim-treesitter/nvim-treesitter'
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
 
   -- Color schemes
   use 'navarasu/onedark.nvim'
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -10,4 +10,38 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
   },
+  textobjects = {
+    select = {
+      enable = true,
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner"
+      }
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer'
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer'
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer'
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer'
+      }
+    }
+  }
 }
