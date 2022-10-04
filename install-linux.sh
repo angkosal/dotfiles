974c0d66bdcde57498cfd757bb9dc21adb8742bc
Ang Kosal <angkosal@gmail.com>
2022-10-04 19:31:58 +0700

    Add install sh for linux

---
+++ install-linux.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e -E
+
+if [ "$#" -ne "1" ]; then
+    echo "Usage: install.sh \$HOME" >&2
+    exit 1
+fi
+
+HERE="$(cd "$(dirname "$0")" && pwd)"
+OUT="$1"
+
+cd "$HERE"
+
+projects=(
+  #i3
+  #i3status
+  #git
+  #rofi
+  alacritty
+  tmux
+  tmuxp
+  nvim
+  zsh
+)
+
+echo "Symlinking files..."
+S="$HERE/symlink.sh"
+for project in "${projects[@]}"; do
+    echo "($project)"
+    HEREP="$HERE/$project"
+    . "$HEREP/install.sh"
+done
+
+echo "Pulling submodules..."
+git submodule --quiet init
+git submodule --quiet update
+
