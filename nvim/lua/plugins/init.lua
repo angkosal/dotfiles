784b8150b03bfec0eb9fb5ec994524710750cd38
Ang Kosal <angkosal@gmail.com>
2023-04-05 17:53:19 +0700

    Moved cmp to dependencies

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -52,12 +52,16 @@ require('lazy').setup({
 	'neovim/nvim-lspconfig',
 
 	-- Autocomplete
-	'L3MON4D3/LuaSnip',
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/cmp-path',
-	'hrsh7th/cmp-buffer',
-	'saadparwaiz1/cmp_luasnip',
-	'hrsh7th/nvim-cmp',
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			'L3MON4D3/LuaSnip',
+			'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-path',
+			'hrsh7th/cmp-buffer',
+			'saadparwaiz1/cmp_luasnip',
+		},
+	},
 
 	-- Statusline
 	'nvim-lualine/lualine.nvim',
