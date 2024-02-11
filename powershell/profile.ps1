05188c642a820500d18cd715a41c6cb08abe885f
Ang Kosal <angkosal@gmail.com>
2024-02-11 19:26:02 +0700

    Chocolatey

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -20,3 +20,13 @@ New-Alias qr Qrcode-Gen
 New-Alias calen Show-Calendar
 New-Alias art Php-Artisan
 New-Alias vi Neovim
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
