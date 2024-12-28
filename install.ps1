48ec7210bb5d2cec30807b7a4e2e515e7e846716
Ang Kosal <angkosal@gmail.com>
2024-12-28 16:04:19 +0700

    Added lazyvim

--- install.ps1
+++ install.ps1
@@ -1,2 +1,2 @@
-New-Item -ItemType SymbolicLink -Path "$HOME/AppData/Local/nvim" -Target "$HOME/dotfiles/nvim"
+New-Item -ItemType SymbolicLink -Path "$HOME/AppData/Local/nvim" -Target "$HOME/dotfiles/lazyvim"
 New-Item -ItemType SymbolicLink -Path "$HOME/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1" -Target "$HOME/dotfiles/powershell/profile.ps1"
