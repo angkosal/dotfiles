f65df443aa18db19bc04afd5b429616267b786f4
Ang Kosal <angkosal@gmail.com>
2024-12-29 11:54:40 +0700

    Kemap

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+vim.keymap.set("n", "<Tab>", "%", { noremap = true, silent = true })
+vim.keymap.set("v", "<Tab>", "%", { noremap = true, silent = true })
